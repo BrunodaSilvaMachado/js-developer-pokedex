5ceed3ceccb2f85f4853512305b9432857961df1
BrunodaSilvaMachado <brunosilvamachado@id.uff.br>
2022-12-07 22:58:50 -0300

    Modal para detalhe foi adicionado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,70 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+// Get the modal
+const modal = document.getElementById("myModal");
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+const modalClose = ()=>{modal.style.display = "none";}
+// When the user clicks on <span> (x), close the modal
+span.onclick = ()=>modalClose();
 
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modalClose();
+    }
+  }
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
+function modalDisplay(id){
+    modal.style.display = "block";
+    const dataPokemon = document.getElementById(id).getAttribute('data-pokemon');
+    const pokemon = JSON.parse(dataPokemon);
+    const number = document.getElementById('number');
+    const name = document.getElementById('name');
+    const height = document.getElementById('height');
+    const weight = document.getElementById('weight');
+    const modalPokemonImg = document.getElementById('modalPokemonImg');
+    const olTypes = document.getElementById('olTypes');
+    const olWeakness = document.getElementById('olWeakness');
+    const olAbilities = document.getElementById('olAbilities');
+    const modalPokemon = document.getElementById('modalPokemon');
+    //tab contents
+    const aboutDetails = document.getElementById('aboutDetails');
+    const olMoves = document.getElementById('olMoves');
+    const pokemonHp = document.getElementById('pokemonHp');
+    const pokemonAtk = document.getElementById('pokemonAtk');
+    const pokemonDef = document.getElementById('pokemonDef');
+    const pokemonSatk = document.getElementById('pokemonSatk');
+    const pokemonSdef = document.getElementById('pokemonSdef');
+    const pokemonSpd = document.getElementById('pokemonSpd');
+    //
+    modalPokemon.className = "";
+    modalPokemon.classList.add("pokemon");
+    modalPokemon.classList.add(pokemon.type);
+    number.innerHTML = pokemon.number;
+    name.innerHTML = pokemon.name;
+    height.innerHTML = pokemon.height;
+    weight.innerHTML = pokemon.weight;
+    olTypes.innerHTML = `${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}`;
+    olWeakness.innerHTML = `${pokemon.weakness.map((weak) => `<li class="type ${pokemon.type}">${weak}</li>`).join('')}`;
+    olAbilities.innerHTML = `${pokemon.abilities.map((ability) => `<li class="type ${pokemon.type}">${ability}</li>`).join('')}`;
+    modalPokemonImg.innerHTML = `<img src="${pokemon.photo}" alt="${pokemon.name}">`;
+    aboutDetails.innerHTML = `${pokemon.about}`;
+    pokemonHp.innerHTML = `${pokemon.hp}`    
+    pokemonAtk.innerHTML = `${pokemon.atk}`;
+    pokemonDef.innerHTML = `${pokemon.def}`;
+    pokemonSatk.innerHTML = `${pokemon.satk}`;
+    pokemonSdef.innerHTML = `${pokemon.sdef}`;
+    pokemonSpd.innerHTML = `${pokemon.satk}`;
+    olMoves.innerHTML = `${pokemon.moves.map((move)=> `<li class="type ${pokemon.type}">${move}</li>`).join('')}`;
+}
+
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li id="${pokemon.number}" class="pokemon ${pokemon.type}" data-toggle="modal" data-target="#myModal" 
+        data-pokemon='${JSON.stringify(pokemon)}' onClick="modalDisplay(${pokemon.number});">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
